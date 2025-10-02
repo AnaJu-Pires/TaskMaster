d4f86aec912bd3c94c892165e17316d3acd82c3a
AnaJu-Pires <anaju001@hotmail.com>
2025-10-02 17:53:45 -0300

    Commit inicial

---
+++ src/main/java/br/ifsp/taskmaster/repository/ContactRepository.java
@@ -0,0 +1,18 @@
+package br.ifsp.taskmaster.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ifsp.taskmaster.model.Contact;
+
+/**
+ * Esta interface extende JpaRepository, que nos fornece métodos prontos 
+ * para acessar e manipular dados no banco de dados. Basta especificar 
+ * a classe de entidade (Contact) e o tipo da chave primária (Long).
+ */
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Page<Contact> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+}
